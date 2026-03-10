ce8a4206fb31dc47579f91779cdf3d8c670c7353
Vårin Sørlie <varinsor@gmail.com>
2026-03-10 17:10:39 +0100

    change name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/personal-website/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {

9c50a552dbfb6b2ab762d1c41044cc926ae89490
Vårin Sørlie <varinsor@gmail.com>
2026-03-10 19:56:30 +0100

    fixing tailwind in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       react: path.resolve(__dirname, "node_modules/react"),
